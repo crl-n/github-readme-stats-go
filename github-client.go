0247bc991d999f534f5cb9155db1a34249fcbefa
Carl Nysten <nysten.carl@gmail.com>
2024-04-28 12:42:58 +0300

    Move repo caching functions to own file repo_cache.go

--- github-client.go
+++ github-client.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"os"
 	"time"
 )
 
@@ -64,52 +63,6 @@ func (rawRepo RepoAPIResponse) ToRepo(ghClient GithubClient) (Repo, error) {
 	repo := Repo{rawRepo.Name, languages, pushedAtTime}
 
 	return repo, nil
-}
-
-const repoCacheFilename = "cached_repos.json"
-
-func CacheRepos(repos []Repo) {
-	file, err := os.Create(repoCacheFilename)
-	if err != nil {
-		fmt.Println("Unable to create " + repoCacheFilename)
-		return
-	}
-	defer file.Close()
-
-	encoder := json.NewEncoder(file)
-	err = encoder.Encode(repos)
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-
-func RetrieveCachedRepos() []Repo {
-	file, err := os.Open(repoCacheFilename)
-	if err != nil {
-		fmt.Println("Unable to open " + repoCacheFilename)
-		return nil
-	}
-
-	var repos []Repo
-	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&repos)
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-
-	return repos
-}
-
-func findRepo(repos []Repo, target RepoAPIResponse) (*Repo, bool) {
-	for _, repo := range repos {
-		if repo.Name == target.Name {
-			fmt.Printf("Cache hit for %v\n", target.Name)
-			return &repo, true
-		}
-	}
-	fmt.Printf("Cache miss for %v\n", target.Name)
-	return nil, false
 }
 
 func (ghClient GithubClient) GetUserRepos() ([]Repo, error) {
---
+++ repo_cache.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+const repoCacheFilename = "cached_repos.json"
+
+func CacheRepos(repos []Repo) {
+	file, err := os.Create(repoCacheFilename)
+	if err != nil {
+		fmt.Println("Unable to create " + repoCacheFilename)
+		return
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(repos)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func RetrieveCachedRepos() []Repo {
+	file, err := os.Open(repoCacheFilename)
+	if err != nil {
+		fmt.Println("Unable to open " + repoCacheFilename)
+		return nil
+	}
+
+	var repos []Repo
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&repos)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	return repos
+}
+
+func findRepo(repos []Repo, target RepoAPIResponse) (*Repo, bool) {
+	for _, repo := range repos {
+		if repo.Name == target.Name {
+			fmt.Printf("Cache hit for %v\n", target.Name)
+			return &repo, true
+		}
+	}
+	fmt.Printf("Cache miss for %v\n", target.Name)
+	return nil, false
+}
