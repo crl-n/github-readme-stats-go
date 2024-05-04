4b483fbd4bdd24906a133589f8177ce1958a5b67
Carl Nysten <nysten.carl@gmail.com>
2024-05-04 10:37:06 +0300

    Add Circle and Rect svg structs, modify SVG struct to take elements as slice of interfaces

--- svg.go
+++ svg.go
@@ -7,17 +7,43 @@ import (
 )
 
 type SVG struct {
-	XMLName xml.Name `xml:"svg"`
+	XMLName  xml.Name      `xml:"svg"`
+	Width    string        `xml:"width,attr"`
+	Height   string        `xml:"height,attr"`
+	Elements []interface{} `xml:",innerxml"`
+}
+
+type Rect struct {
+	XMLName xml.Name `xml:"rect"`
 	Width   string   `xml:"width,attr"`
 	Height  string   `xml:"height,attr"`
-	Content string   `xml:",innerxml"`
+	Fill    string   `xml:"fill,attr"`
+}
+
+type Circle struct {
+	XMLName     xml.Name `xml:"circle"`
+	Cx          string   `xml:"cx,attr"`
+	Cy          string   `xml:"cy,attr"`
+	R           string   `xml:"r,attr"`
+	Stroke      string   `xml:"stroke,attr"`
+	StrokeWidth string   `xml:"stroke-width,attr"`
+	Fill        string   `xml:"fill,attr"`
 }
 
 func GenerateTestSVG() {
 	svg := &SVG{
-		Width:   "100",
-		Height:  "100",
-		Content: `<circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />`,
+		Width:  "100",
+		Height: "100",
+		Elements: []interface{}{
+			Rect{
+				Width:  "100",
+				Height: "100",
+				Fill:   "red",
+			},
+			Circle{
+				Cx: "50", Cy: "50", R: "40", Stroke: "green", StrokeWidth: "4", Fill: "yellow",
+			},
+		},
 	}
 
 	output, err := xml.MarshalIndent(svg, "", " ")
