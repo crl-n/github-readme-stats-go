e3df385a610adfdfe867d5c857b5035d991aff9c
Carl Nysten <nysten.carl@gmail.com>
2024-08-05 21:35:23 +0300

    Allow Github handle to be specified as environment variable or as command line argument

--- main.go
+++ main.go
@@ -10,19 +10,49 @@ import (
 	"github.com/crl-n/github-readme-stats-go/svg"
 )
 
+const (
+	githubHandleEnvVar = "GITHUB_HANDLE"
+)
+
 func usage() {
 	fmt.Println("Usage:")
-	fmt.Println(os.Args[0] + " gh\t\tGet and display Github stats")
-	fmt.Println(os.Args[0] + " lang\t\tGenerate most used languages card")
+	fmt.Println(os.Args[0] + " gh [handle]\t\tGet and display Github stats")
+	fmt.Println(os.Args[0] + " lang [handle]\t\tGenerate most used languages card")
+}
+
+// Gets Github handle to be used. If no Github handle provided as command line
+// argument, environment variable is used. If no handle provided, exit gracefully.
+func getHandle() string {
+	var usedValue string
+
+	if len(os.Args) == 3 {
+		argValue := os.Args[2]
+		usedValue = argValue
+	} else {
+		envValue := os.Getenv(githubHandleEnvVar)
+
+		if envValue == "" {
+			logger.Errorf(
+				"Error: No Github handle provided. Provide handle as argument or "+
+					"set environment variable '%s'\n", githubHandleEnvVar,
+			)
+			os.Exit(1)
+		}
+		usedValue = envValue
+	}
+
+	logger.Infof("Using Github handle '%s'\n", usedValue)
+	return usedValue
 }
 
 func main() {
-	if len(os.Args) != 2 {
+	if len(os.Args) < 2 {
 		usage()
 		return
 	}
 
-	client := githubclient.New("crl-n")
+	githubHandle := getHandle()
+	client := githubclient.New(githubHandle)
 
 	switch os.Args[1] {
 	case "lang":
